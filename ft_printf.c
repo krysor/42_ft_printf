214da6f287417c46a64291a2c36a789124b38a9e
Krystian Kaczorowski <kkaczoro@shi-r4-p1.s19.be>
2022-04-20 19:08:56 +0200

    substitution working

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,43 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 18:49:48 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/04/14 14:13:48 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/04/20 19:08:11 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
-int	ft_printf(const char *smth, ...)
+static char	*ft_subst(char *str, char c1, char c2)
+{
+	int		i;
+	int		p;
+
+	i = 0;
+	p = 0;
+	while (str[i])
+	{
+		if (str[i] == c1 && p == 0)
+		{
+			str[i] = c2;
+			p = 1;
+		}
+		else
+			p = 0;
+		i++;
+	}
+	return (str);
+}
+
+int	ft_printf(const char *s, ...)
 {
 	va_list	ap;
+	char	*str;
+	char	**arr;
 
+	str = ft_subst(ft_strdup(), '%', 127);
+	arr = split(str);
+	if (arr == 0)
+		return (1);
 	va_start(ap, 0);
 	va_end(ap);
 	return (0);
@@ -42,6 +69,7 @@ practical:
 		1.1 copy the original input str
 		1.2 if last c isnt %
 			1.2.1 replace % by SMTH ELSE
+				127 = DEL
 		1.3 use SMTH ELSE as split separator
 			1.3.1 CHECK IF THERES AN ANOTHER ASCII CHARACTER LIKE "%" THAT HAS NO EFFECT IN PRINTF
 	2. copy flags into temp char
--- test.c
+++ test.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main()
+static char	*ft_subst(char *str, char c1, char c2)
 {
-    printf("%.d", 1345345);
-	return 0;
+	int		i;
+	int		p;
+
+	i = 0;
+	p = 0;
+	while (str[i])
+	{
+		if (str[i] == c1 && p == 0)
+		{
+			str[i] = c2;
+			p = 1;
+		}
+		else
+			p = 0;
+		i++;
+	}
+	return (str);
+}
+
+int	main()
+{
+	char str[] = "%dklasdnasjk%cvvdv%%dcjnscjds%i%%";
+	printf("%s", ft_subst(str, '%', 'O'));
+
+	return (0);
 }
