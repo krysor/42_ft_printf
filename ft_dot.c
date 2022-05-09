ac42214d1210b3bdf755741a8ed273ef51ba6a96
Krystian Kaczorowski <kkaczoro@mi-r1-p3.s19.be>
2022-05-09 16:13:32 +0200

    dot not working in another case

--- ft_dot.c
+++ ft_dot.c
@@ -6,11 +6,14 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:19:09 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/05/09 12:25:18 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/05/09 16:12:49 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+static int	ft_dotornot(const char *s, char *old_str, int ind_conv, int *prec);
+static void	ft_negative(char *new_str);
 
 char	*ft_dot(const char *s, char *old_str, int ind_conv)
 {
@@ -18,15 +21,10 @@ char	*ft_dot(const char *s, char *old_str, int ind_conv)
 	int			precision;
 
 	new_str = (char *)ft_memchr((void *)s, '.', (size_t)ind_conv);
+	precision = 0;
 	if (new_str != 0)
 		precision = ft_atoi(new_str + 1);
-	else
-		precision = 0;
-	if (!ft_isflag(s, '.', s[ind_conv]) || s[ind_conv] == 'c' || precision < 0
-		|| ((s[ind_conv] == 'd' || s[ind_conv] == 'i' || s[ind_conv] == 'o'
-				|| s[ind_conv] == 'u' || s[ind_conv] == 'x'
-				|| s[ind_conv] == 'X') && precision <= (int)ft_strlen(old_str))
-		|| (s[ind_conv] == 's' && precision >= (int)ft_strlen(old_str)))
+	if (ft_dotornot(s, old_str, ind_conv, &precision))
 		return (old_str);
 	new_str = (char *)malloc(sizeof(char) * precision + 1);
 	if (new_str == NULL)
@@ -34,11 +32,44 @@ char	*ft_dot(const char *s, char *old_str, int ind_conv)
 	if (s[ind_conv] != 's')
 	{
 		(void)ft_memset((void *)new_str, '0', precision - ft_strlen(old_str));
-		precision -= ft_strlen(old_str);
-		(void)ft_memmove((void *)(new_str + precision), old_str, ft_strlen(old_str));
+		(void)ft_strlcpy((char *)(new_str + precision - ft_strlen(old_str)),
+			old_str, ft_strlen(old_str) + 1);
 	}
 	else
-		(void)ft_memmove((void *)new_str, old_str, precision);
+		(void)ft_strlcpy((char *)(new_str), old_str, precision + 1);
+	if ((s[ind_conv] == 'd' || s[ind_conv] == 'i') && old_str[0] == '-')
+		ft_negative((char *)new_str);
 	free(old_str);
 	return ((char *)new_str);
+}
+
+static int	ft_dotornot(const char *s, char *old_str, int ind_conv, int *prec)
+{
+	char	conv;
+
+	conv = s[ind_conv];
+	if ((!ft_isflag(s, '.', conv) || conv == 'c' || *prec < 0
+			|| ((conv == 'd' || conv == 'i' || conv == 'o' || conv == 'u'
+					|| conv == 'x' || conv == 'X')
+				&& *prec <= (int)ft_strlen(old_str))
+			|| (conv == 's' && *prec >= (int)ft_strlen(old_str))))
+	{
+		if ((conv == 'd' || conv == 'i') && old_str[0] == '-'
+			&& *prec + 1 > (int)ft_strlen(old_str))
+		{	
+			(*prec)++;
+			return (0);
+		}
+		return (1);
+	}
+	return (0);
+}
+
+static void	ft_negative(char *new_str)
+{
+	new_str[0] = '-';
+	new_str++;
+	while (*new_str != '-')
+		new_str++;
+	*new_str = '0';
 }
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 13:13:58 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/05/06 18:17:50 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/05/09 16:11:20 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,14 @@ int	main(void)
 */
 
 //te checken: c, s, p, d, i, u, x, X
+
 int	main(void)
 {
 	//printf("%lu\n", ULONG_MAX);
 	//const char *str = "%.10s";
 	//printf("%s", ft_memchr(str, '.', (size_t)4));
-	printf("%d\n", printf(" %.s \n", "-"));
-	printf("%d\n", ft_printf(" %.s \n", "-"));
+
+	printf("%d\n", printf(" %.4d \n", -14));
+	printf("%d\n", ft_printf(" %.4d \n", -14));
 	return (0);
 }
