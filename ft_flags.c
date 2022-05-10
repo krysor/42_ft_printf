5634f0ce3c54db5d0c81d4944aeca30987ac2491
Krystian Kaczorowski <kkaczoro@mi-r7-p1.s19.be>
2022-05-10 11:39:12 +0200

    working on mfv in combo with 0 and -

--- ft_flags.c
+++ ft_flags.c
@@ -6,19 +6,21 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:23:02 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/05/10 10:56:27 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/05/10 11:37:59 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
 char	*ft_sign(const char *s, char *old_str, int i_conv);
+char	*ft_field(const char *s, char *old_str, int i_conv);
 
 char	*ft_flag(const char *s, char *old_str, int i_conv)
 {
-	old_str = ft_dot(s, old_str, i_conv);//protect everything from NULL result
+	old_str = ft_dot(s, old_str, i_conv);//protect everything from NULL result?
 	old_str = ft_hash(s, old_str, s[i_conv]);
 	old_str = ft_sign(s, old_str, i_conv);//control sign AFTER making MFV work
+	//old_str = ft_flags(s, old_str, i_conv);
 	return (old_str);
 }
 
@@ -37,67 +39,45 @@ char	*ft_sign(const char *s, char *old_str, int i_conv)
 {
 	int		plus;
 	int		space;
-	int		len_str;
+	int		len_old;
 	char	*new_str;
 
 	space = ft_isflag(s, ' ', s[i_conv]);
 	plus = ft_isflag(s, '+', s[i_conv]);
-	len_str = ft_strlen(old_str);
+	len_old = ft_strlen(old_str);
 	if ((s[i_conv] != 'i' && s[i_conv] != 'd') || old_str[0] == '-'
 		|| (!space && !plus))
 		return (old_str);
-	new_str = (char *)malloc(sizeof(char) * len_str + 2);
+	new_str = (char *)malloc(sizeof(char) * len_old + 2);
 	if (new_str == NULL)
 		return (NULL);
 	new_str[0] = ' ';
 	if (plus)
 		new_str[0] = '+';
-	(void)ft_strlcpy(new_str + 1, old_str, len_str + 1);
+	(void)ft_strlcpy(new_str + 1, old_str, len_old + 1);
 	free(old_str);
 	return (new_str);
 }
 
-/*
-int	ft_isflag(const char *s, char flag, char conv)
+char	*ft_field(const char *s, char *old_str, int i_conv)
 {
-	while (*s != conv)
-	{
-		if (*s == flag)
-			return (1);
+	int		field;
+	int		len_old;
+	char	*new_str;
+
+	while (*s == '-' || *s == '0' || *s == '#' || *s == ' ' || *s == '+')
 		s++;
-	}
-	return (0);
-}
-
-char	*ft_sign(const char *s, char *old_str, int i_conv)
-{
-	int		plus;
-	int		space;
-	int		len_str;
-	char	*new_str;
-
-	space = ft_isflag(s, ' ', s[i_conv]);
-	plus = ft_isflag(s, '+', s[i_conv]);
-	len_str = ft_strlen(old_str);
-	if (s[i_conv] == 's' && len_str == 0 && space)
-	{
-		new_str = (char *)malloc(sizeof(char) * 2);
-		if (new_str == NULL)
-			return (NULL);
-		new_str[0] = ' ';
-		return (new_str);
-	}
-	if ((s[i_conv] != 'i' && s[i_conv] != 'd') || old_str[0] == '-'
-		|| (!space && !plus))
+	new_str = (char *)ft_memchr((void *)s, '.', (size_t)i_conv);
+	field = 0;
+	if (new_str != 0)
+		field = ft_atoi(new_str + 1);
+	len_old = (int)ft_strlen(old_str);
+	if (!field || field <= len_old)
 		return (old_str);
-	new_str = (char *)malloc(sizeof(char) * len_str + 2);
+	new_str = (char *)malloc(sizeof(char) * field + 1);
 	if (new_str == NULL)
 		return (NULL);
-	new_str[0] = ' ';
-	if (plus)
-		new_str[0] = '+';
-	(void)ft_strlcpy(new_str + 1, old_str, len_str + 1);
+	
 	free(old_str);
 	return (new_str);
 }
-*/
--- ft_hash.c
+++ ft_hash.c
@@ -6,7 +6,7 @@
 /*   By: kkaczoro <kkaczoro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:12:14 by kkaczoro          #+#    #+#             */
-/*   Updated: 2022/05/09 14:52:44 by kkaczoro         ###   ########.fr       */
+/*   Updated: 2022/05/10 11:23:05 by kkaczoro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 char	*ft_hash(const char *s, char *old_str, char conv)
 {
 	char	*new_str;
-	size_t	str_len;
+	size_t	str_old;
 
 	if ((conv != 'p' && ((conv != 'x' && conv != 'X')
 				|| !ft_isflag(s, '#', conv)))
 		|| (conv != 'p' && ft_strlen(old_str) == 1 && old_str[0] == '0'))
 		return (old_str);
-	str_len = ft_strlen(old_str);
-	new_str = (char *)malloc(sizeof(char) * str_len + 3);
+	str_old = ft_strlen(old_str);
+	new_str = (char *)malloc(sizeof(char) * str_old + 3);
 	if (new_str == NULL)
 		return (NULL);
 	new_str[0] = '0';
@@ -30,7 +30,7 @@ char	*ft_hash(const char *s, char *old_str, char conv)
 		new_str[1] = conv;
 	else
 		new_str[1] = 'x';
-	(void)ft_strlcpy(new_str + 2, old_str, str_len + 1);
+	(void)ft_strlcpy(new_str + 2, old_str, str_old + 1);
 	free(old_str);
 	return (new_str);
 }
